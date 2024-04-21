690212e409446814a31a38b019fbfe84a6f0b934
Diegonzalez Dev <diegonzalezdev@gmail.com>
2024-04-21 16:19:17 -0300

    continuo actividad 2/2

--- Semana 05/ScriptActividad2-2.sql
+++ Semana 05/ScriptActividad2-2.sql
@@ -30,7 +30,7 @@ use Univ_Angel
 --     ELSE d.Domicilio
 -- END as 'Informacion de Contacto'
 -- FROM Usuarios u
--- INNER JOIN Datos_Personales d ON u.ID = d.ID
+-- INNER JOIN Datos_Personales d ON u.ID = d.ID 
 
 --5 Listado con apellido y nombres, nombre del curso y costo de la inscripción de todos los usuarios inscriptos a cursos.
 -- NOTA: No deben figurar los usuarios que no se inscribieron a ningún curso.
@@ -57,7 +57,7 @@ use Univ_Angel
 -- INNER JOIN Pagos p on i.ID = p. IDInscripcion
 
 -- 8 Listado con nombre y apellidos, género, fecha de nacimiento, mail, nombre del curso y fecha de certificación de todos aquellos usuarios que se hayan certificado.
--- SELECT d.Nombres, d.Apellidos, d.Nacimiento, d.Email, cu.Nombre as 'Nombre del curso', c.Fecha as 'Fecha de certificacion'
+-- SELECT d.Nombres, d.Apellidos, d.Nacimiento, d.Genero, d.Email, cu.Nombre as 'Nombre del curso', c.Fecha as 'Fecha de certificacion'
 -- FROM Datos_Personales d 
 -- INNER JOIN Inscripciones i on d.ID = i.IDUsuario
 -- INNER JOIN Cursos cu on cu.ID = i.IDCurso
@@ -82,4 +82,41 @@ use Univ_Angel
 -- INNER JOIN Categorias c on c.ID = ca.IDCategoria
 -- WHERE c.Nombre = 'Historia'
 
---12
+-- 12 Listado con nombre de idioma, código de curso y código de tipo de idioma. Listar todos los idiomas indistintamente si no tiene cursos relacionados.
+-- SELECT i.Nombre as 'Idioma', c.ID as 'Codigo de Curso', ic.IDFormatoIdioma as 'Codigo tipo de idioma'
+-- FROM Idiomas i 
+-- LEFT JOIN Idiomas_x_Curso ic ON i.ID = IC.IDIdioma
+-- LEFT JOIN Cursos c ON c.ID = ic.IDCurso
+
+-- 13 Listado con nombre de idioma de todos los idiomas que no tienen cursos relacionados.
+-- SELECT i.Nombre as 'Idioma'
+-- FROM Idiomas i 
+-- LEFT JOIN Idiomas_x_Curso ic on i.ID = ic.IDIdioma
+-- WHERE ic.IDIdioma is null
+
+-- 14 Listado con nombres de idioma que figuren como audio de algún curso. Sin repeticiones.
+-- SELECT DISTINCT i.Nombre
+-- FROM Idiomas i 
+-- INNER JOIN Idiomas_x_Curso ic ON i.ID = ic.IDIdioma
+-- INNER JOIN FormatosIdioma fi ON ic.IDFormatoIdioma = fi.ID
+-- WHERE fi.Nombre = 'Audio'
+
+-- 15 Listado con nombres y apellidos de todos los usuarios y el nombre del país en el que nacieron. Listar todos los países indistintamente si no tiene usuarios relacionados.
+-- SELECT d.Nombres, d.Apellidos, p.Nombre as 'Pais'
+-- FROM Datos_Personales d 
+-- INNER JOIN Localidades l on l.ID = d.IDLocalidad
+-- RIGHT JOIN Paises p on l.IDPais = p.ID
+
+-- 16 Listado con nombre de curso, fecha de estreno y nombres de usuario de todos los inscriptos. Listar todos los nombres de usuario indistintamente si no se inscribieron a ningún curso.
+-- SELECT c.Nombre as 'Nombre del Curso', c.Estreno as 'Fecha de Estreno', u.NombreUsuario as 'Nombre de usuario'
+-- FROM Cursos c 
+-- INNER JOIN Inscripciones i on c.ID = i.IDCurso
+-- RIGHT JOIN Usuarios u on i.IDUsuario = u.ID
+
+-- 17 Listado con nombre de usuario, apellido, nombres, género, fecha de nacimiento y mail de todos los usuarios que no cursaron ningún curso.
+-- SELECT u.NombreUsuario, d.Apellidos, d.Nombres, d.Genero, d.Nacimiento, d.Email
+-- FROM Usuarios u
+-- LEFT JOIN Datos_Personales d on u.ID = d.ID
+-- LEFT JOIN Inscripciones i on i.IDUsuario = d.ID
+-- WHERE i.IDUsuario is null 
+
